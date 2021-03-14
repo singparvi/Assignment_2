f29bdb9443ccae56fd8447e31023b85467c83823
Parvi <sing.parvi1@gmail.com>
2021-03-14 14:31:18 -0400

    2021-03-14 Commit after making progress in assignment

--- helper_functions.py
+++ helper_functions.py
@@ -13,5 +13,41 @@ def split_dates(date_series):
     output = pd.DataFrame(frame)
     print(output)
 
+class CustomDF(pd.DataFrame):
+    def __init__(self, filename):
+        super().__init__(pd.read_csv(filename))
 
-# adding new line for new branch test
+    # print('Class Instantiated')
+
+    def nullcount(self):
+        print('This code is getting run')
+        return self.isnull().sum().sum()
+
+    def split_dates(self):
+        month = self.dt.month
+        day = self.dt.day
+        year = self.dt.year
+        frame = {"month": month, "day": day, "year": year}
+        output = self.DataFrame(frame)
+        return output
+
+if __name__ == '__main__':
+
+    # instantiating object for the purpose of testing nullcount method in class CustomDF
+    print('Running First line')
+    # import file
+    df_forest = CustomDF('ForestCover.csv')
+    print('First line of code executed')
+    # new_df_obj = New_DataFrame(df_new)
+    # null_counted = new_df_obj.nullcount()
+    print('DF: ', df_forest)
+    #test method
+    nullcounts = df_forest.nullcount()
+
+    #instantiating another object for the purpose of testing split_dates method in class CustomDF
+    #import file
+    df_dates = CustomDF('Dates.csv')
+    # test method
+    df_dates_changed = df_dates.split_dates()
+    print('The new df with changed dates is:', df_dates_changed)
+
